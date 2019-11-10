a0cc67d87d9db8484a4d79e8ebfa5c62121974ec
Elin Sexton <elin.sexton@gmail.com>
2019-11-10 19:52:12 +0100

    Minor adjustments; String SQL, Logger etc

--- SchoolProject-student/src/main/java/se/alten/schoolproject/transaction/StudentTransaction.java
+++ SchoolProject-student/src/main/java/se/alten/schoolproject/transaction/StudentTransaction.java
@@ -1,6 +1,6 @@
 package se.alten.schoolproject.transaction;
 
-
+import org.jboss.logging.Logger;
 import se.alten.schoolproject.entity.Student;
 import se.alten.schoolproject.model.StudentModel;
 
@@ -10,13 +10,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
 import javax.persistence.Query;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Stateless
 @Default
 public class StudentTransaction implements StudentTransactionAccess{
+
+    public static final Logger logger = Logger.getLogger(
+            StudentTransaction.class.getName());
 
     @PersistenceContext(unitName="school")
     private EntityManager entityManager;
@@ -26,14 +33,15 @@ public class StudentTransaction implements StudentTransactionAccess{
         //return entityManager.createNamedQuery("Student.findAll", Student.class).getResultList();
         String select = "SELECT s from Student s";
         Query query = entityManager.createQuery(select);
+
         return query.getResultList();
     }
 
     public List listStudentByName(String student){
-
         String selectName = "SELECT s FROM Student s WHERE s.firstname = :firstname";
         Query query = entityManager.createQuery(selectName);
 
+        System.out.println(logger);
         return query.setParameter("firstname", student).getResultList();
     }
 
@@ -51,8 +59,9 @@ public class StudentTransaction implements StudentTransactionAccess{
 
     @Override
     public void removeStudent(String student) {
+        String deleteStudent = "DELETE FROM Student s WHERE s.email = :email";
         //JPQL Query
-        Query query = entityManager.createQuery("DELETE FROM Student s WHERE s.email = :email");
+        Query query = entityManager.createQuery(deleteStudent);
 
         //Native Query
         //Query query = entityManager.createNativeQuery("DELETE FROM student WHERE email = :email", Student.class);
@@ -63,7 +72,8 @@ public class StudentTransaction implements StudentTransactionAccess{
 
     @Override
     public void updateStudent(String firstname, String lastname, String email) {
-        Query updateQuery = entityManager.createNativeQuery("UPDATE student SET firstname = :firstname, lastname = :lastname WHERE email = :email", Student.class);
+        String updateStudent = "UPDATE student SET firstname = :firstname, lastname = :lastname WHERE email = :email";
+        Query updateQuery = entityManager.createNativeQuery(updateStudent, Student.class);
         updateQuery.setParameter("firstname", firstname)
                    .setParameter("lastname", lastname)
                    .setParameter("email", email)
@@ -72,10 +82,12 @@ public class StudentTransaction implements StudentTransactionAccess{
 
     @Override
     public void updateStudentPartial(Student student) {
-        Student studentFound = (Student)entityManager.createQuery("SELECT s FROM Student s WHERE s.email = :email")
+        String selectStudentToUpdate = "SELECT s FROM Student s WHERE s.email = :email";
+        String updateFirstName = "UPDATE Student SET firstname = :studentFirstname WHERE email = :email";
+        Student studentFound = (Student)entityManager.createQuery(selectStudentToUpdate)
                 .setParameter("email", student.getEmail()).getSingleResult();
 
-        Query query = entityManager.createQuery("UPDATE Student SET firstname = :studentFirstname WHERE email = :email");
+        Query query = entityManager.createQuery(updateFirstName);
         query.setParameter("studentFirstname", student.getFirstname())
                 .setParameter("email", studentFound.getEmail())
                 .executeUpdate();
